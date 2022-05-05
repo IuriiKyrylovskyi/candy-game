2a3617f635f422ef6eb4691a5c718a0ec63d9b4a
Yurii Kyrylovskyi <iurii712@ukr.net>
2022-05-05 23:17:04 +0300

    Create 5 for desktop

--- candy.js
+++ candy.js
@@ -7,13 +7,236 @@ const columns = 9
 let board = []
 let curCandy
 let otherCandy
-
-const scoreElement = document.querySelector('#score')
+let score = 0
 
 const setRandom = arr => {
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
+const checkValid = () => {
+  //check rows
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 2; c++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r][c + 1]
+      const candy03 = board[r][c + 2]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy01Name !== BLANK
+      ) {
+        return true
+      }
+    }
+  }
+
+  //check columns
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 2; r++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r + 1][c]
+      const candy03 = board[r + 2][c]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy01Name !== BLANK
+      ) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+const createPop = (element, num) => {
+  const coordinates = element.getBoundingClientRect()
+
+  const popScore = document.createElement('div')
+  popScore.classList.add('pop-score')
+  popScore.textContent = `+${num}`
+  popScore.style.top = `${coordinates.top}px`
+  popScore.style.left = `${coordinates.left}px`
+  popScore.classList.add('visible')
+
+  document.querySelector('main').appendChild(popScore)
+
+  const timeout = setTimeout(() => {
+    popScore.remove()
+  }, 750)
+
+  return () => clearTimeout(timeout)
+}
+
+const cruchFive = () => {
+  //check rows
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 4; c++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r][c + 1]
+      const candy03 = board[r][c + 2]
+      const candy04 = board[r][c + 3]
+      const candy05 = board[r][c + 4]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+      const candy04Name = candy04.getAttribute('data-name')
+      const candy05Name = candy05.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy03Name === candy04Name &&
+        candy04Name === candy05Name &&
+        candy01Name !== BLANK
+      ) {
+        candy01.setAttribute('data-name', BLANK)
+        candy01.setAttribute('src', BLANK_IMG_PATH)
+
+        candy02.setAttribute('data-name', BLANK)
+        candy02.setAttribute('src', BLANK_IMG_PATH)
+
+        candy03.setAttribute('data-name', BLANK)
+        candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        candy04.setAttribute('data-name', BLANK)
+        candy04.setAttribute('src', BLANK_IMG_PATH)
+
+        candy05.setAttribute('data-name', BLANK)
+        candy05.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 20
+
+        createPop(candy03, 20)
+      }
+    }
+  }
+
+  //check columns
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 4; r++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r + 1][c]
+      const candy03 = board[r + 2][c]
+      const candy04 = board[r + 3][c]
+      const candy05 = board[r + 4][c]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+      const candy04Name = candy04.getAttribute('data-name')
+      const candy05Name = candy05.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy03Name === candy04Name &&
+        candy04Name === candy05Name &&
+        candy01Name !== BLANK
+      ) {
+        candy01.setAttribute('data-name', BLANK)
+        candy01.setAttribute('src', BLANK_IMG_PATH)
+
+        candy02.setAttribute('data-name', BLANK)
+        candy02.setAttribute('src', BLANK_IMG_PATH)
+
+        candy03.setAttribute('data-name', BLANK)
+        candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        candy04.setAttribute('data-name', BLANK)
+        candy04.setAttribute('src', BLANK_IMG_PATH)
+
+        candy05.setAttribute('data-name', BLANK)
+        candy05.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 20
+
+        createPop(candy03, 20)
+      }
+    }
+  }
+}
+const cruchFour = () => {
+  //check rows
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r][c + 1]
+      const candy03 = board[r][c + 2]
+      const candy04 = board[r][c + 3]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+      const candy04Name = candy04.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy03Name === candy04Name &&
+        candy01Name !== BLANK
+      ) {
+        candy01.setAttribute('data-name', BLANK)
+        candy01.setAttribute('src', BLANK_IMG_PATH)
+
+        candy02.setAttribute('data-name', BLANK)
+        candy02.setAttribute('src', BLANK_IMG_PATH)
+
+        candy03.setAttribute('data-name', BLANK)
+        candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        candy04.setAttribute('data-name', BLANK)
+        candy04.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 15
+
+        createPop(candy02, 15)
+      }
+    }
+  }
+
+  //check columns
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+      const candy01 = board[r][c]
+      const candy02 = board[r + 1][c]
+      const candy03 = board[r + 2][c]
+      const candy04 = board[r + 3][c]
+      const candy01Name = candy01.getAttribute('data-name')
+      const candy02Name = candy02.getAttribute('data-name')
+      const candy03Name = candy03.getAttribute('data-name')
+      const candy04Name = candy04.getAttribute('data-name')
+
+      if (
+        candy01Name === candy02Name &&
+        candy02Name === candy03Name &&
+        candy03Name === candy04Name &&
+        candy01Name !== BLANK
+      ) {
+        candy01.setAttribute('data-name', BLANK)
+        candy01.setAttribute('src', BLANK_IMG_PATH)
+
+        candy02.setAttribute('data-name', BLANK)
+        candy02.setAttribute('src', BLANK_IMG_PATH)
+
+        candy03.setAttribute('data-name', BLANK)
+        candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        candy04.setAttribute('data-name', BLANK)
+        candy04.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 15
+
+        createPop(candy02, 15)
+      }
+    }
+  }
+}
 const cruchThree = () => {
   //check rows
   for (let r = 0; r < rows; r++) {
@@ -38,6 +261,10 @@ const cruchThree = () => {
 
         candy03.setAttribute('data-name', BLANK)
         candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 10
+
+        createPop(candy02, 10)
       }
     }
   }
@@ -65,14 +292,57 @@ const cruchThree = () => {
 
         candy03.setAttribute('data-name', BLANK)
         candy03.setAttribute('src', BLANK_IMG_PATH)
+
+        score += 10
+
+        createPop(candy02, 10)
       }
+    }
+  }
+}
+
+const crushCandy = () => {
+  cruchFive()
+  cruchFour()
+  cruchThree()
+  document.querySelector('#score').textContent = score
+}
+
+const slideCandy = () => {
+  for (let c = 0; c < columns; c++) {
+    let ind = rows - 1
+    for (let r = rows - 1; r >= 0; r--) {
+      if (board[r][c].getAttribute('data-name') !== BLANK) {
+        board[ind][c].setAttribute(
+          'data-name',
+          board[r][c].getAttribute('data-name')
+        )
+        board[ind][c].src = board[r][c].src
+
+        ind -= 1
+      }
+    }
+
+    for (let r = ind; r >= 0; r--) {
+      board[r][c].setAttribute('data-name', BLANK)
+      board[r][c].src = BLANK_IMG_PATH
+    }
+  }
+}
+
+const generateCandy = () => {
+  for (let c = 0; c < columns; c++) {
+    if (board[0][c].getAttribute('data-name') === BLANK) {
+      const candy = setRandom(candies)
+
+      board[0][c].setAttribute('data-name', candy)
+      board[0][c].src = `./images/${candy}.png`
     }
   }
 }
 
 const onDragstart = e => {
   curCandy = e.target
-  console.log(curCandy)
 }
 const onDragenter = e => {
   e.preventDefault()
@@ -84,17 +354,17 @@ const onDragleave = e => {
   e.preventDefault()
 }
 const onDrop = e => {
-  console.log(e.target)
   otherCandy = e.target
 }
 
-const crushCandy = () => {
-  // cruchFive()
-  // cruchFive()
-  cruchThree()
-}
-
 const onDragend = () => {
+  // prevent swap with blank
+  if (
+    curCandy.getAttribute('data-name') === BLANK ||
+    otherCandy.getAttribute('data-name') === BLANK
+  )
+    return
+
   const curCoors = curCandy.id.split('-')
   const r = +curCoors[0]
   const c = +curCoors[1]
@@ -116,18 +386,31 @@ const onDragend = () => {
       const curName = curCandy.getAttribute('data-name')
       const otherName = otherCandy.getAttribute('data-name')
 
-      // prevent swap with blank
-      if (curName === BLANK || otherName === BLANK) return
-
       curCandy.src = otherImg
       otherCandy.src = curImg
 
       curCandy.setAttribute('data-name', otherName)
       otherCandy.setAttribute('data-name', curName)
+
+      const isValid = checkValid()
+
+      if (!isValid) {
+        const curImg = curCandy.src
+        const otherImg = otherCandy.src
+        const curName = curCandy.getAttribute('data-name')
+        const otherName = otherCandy.getAttribute('data-name')
+
+        curCandy.src = otherImg
+        otherCandy.src = curImg
+
+        curCandy.setAttribute('data-name', otherName)
+        otherCandy.setAttribute('data-name', curName)
+        return
+      }
     }
   }
 
-  const timeout = setTimeout(swapImgs, 100)
+  const timeout = setTimeout(swapImgs, 200)
 
   return () => clearTimeout(timeout)
 }
@@ -161,7 +444,13 @@ const startGame = () => {
 window.onload = () => {
   startGame()
 
-  const interval = setInterval(cruchThree, 100)
+  const interval = setInterval(() => {
+    crushCandy()
+    slideCandy()
+    generateCandy()
+  }, 100)
+
+  setInterval(() => {}, interval)
 
   return () => clearInterval(interval)
 }
